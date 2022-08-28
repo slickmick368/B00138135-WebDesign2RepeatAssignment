5e6a009296df3e133bf7a584d788769722dbc137
Michael Heavey <42016654+slickmick368@users.noreply.github.com>
2022-08-28 16:45:13 +0100

    Add files via upload

---
+++ js/merchScript.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
